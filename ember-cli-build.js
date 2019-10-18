09289b9aedea001e2887ca37311086d712119bcc
Alex LaFroscia <alex@lafroscia.com>
2019-10-18 13:06:26 -0400

    feat: configure project to use Tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,32 @@
 "use strict";
 
 const EmberApp = require("ember-cli/lib/broccoli/ember-app");
+const isProduction = EmberApp.env() === "production";
+
+const purgeCSS = {
+  module: require("@fullhuman/postcss-purgecss"),
+  options: {
+    content: [
+      "./app/index.html",
+      "./app/components/**/*.hbs",
+      "./app/templates/**/*.hbs"
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+};
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          require("postcss-import"),
+          require("tailwindcss")("./config/tailwind.js"),
+          ...(isProduction ? [purgeCSS] : [])
+        ]
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
