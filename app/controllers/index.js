3e90d6f5c515bf2b6c23501430d8377190b8a071
Alex LaFroscia <alex@lafroscia.com>
2019-11-11 15:34:39 -0500

    feat: show number of installed dependencies

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,15 @@
-import Controller from "@ember/controller";
 import { computed } from "@ember/object";
-import { inject as service } from "@ember/service";
 
-export default class IndexController extends Controller {
-  @service lockfile;
+import InstalledController from "./installed";
 
+export default class IndexController extends InstalledController {
   @computed("lockfile.parsedValue")
-  get dependencyCount() {
+  get totalDependencyCount() {
     return Object.entries(this.lockfile.parsedValue).length;
+  }
+
+  @computed("installedDependencies")
+  get installedDependencyCount() {
+    return Object.entries(this.installedDependencies).length;
   }
 }
