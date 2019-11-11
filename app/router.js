eae594e35e2847c72f3c85236857a01562981f12
Alex LaFroscia <alex@lafroscia.com>
2019-11-11 15:36:20 -0500

    chore: fix linting issues

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route("all");
-  this.route('installed');
+  this.route("installed");
 });
 
 export default Router;
--- tests/unit/controllers/installed-test.js
+++ tests/unit/controllers/installed-test.js
@@ -1,12 +1,12 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 
-module('Unit | Controller | installed', function(hooks) {
+module("Unit | Controller | installed", function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:installed');
+  test("it exists", function(assert) {
+    let controller = this.owner.lookup("controller:installed");
     assert.ok(controller);
   });
 });
