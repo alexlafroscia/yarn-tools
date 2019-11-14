5685b08c7f40da2affc6318760c03cad52db8a16
Alex LaFroscia <alex@lafroscia.com>
2019-11-14 11:10:09 -0500

    feat: add filtering to `installed` tab

--- app/controllers/installed.js
+++ app/controllers/installed.js
@@ -1,13 +1,19 @@
 import Controller from "@ember/controller";
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
 import { assembleVersion, parseVersion } from "../utils/version-utils";
 
 export default class InstalledController extends Controller {
   @service lockfile;
+  @tracked filter = "";
 
   get installedDependencies() {
-    return Object.entries(this.lockfile.parsedValue).reduce(
-      (acc, [key, value]) => {
+    const filter = this.filter.toLowerCase();
+
+    return Object.entries(this.lockfile.parsedValue)
+      .filter(([key]) => key.toLowerCase().indexOf(filter) > -1)
+      .reduce((acc, [key, value]) => {
         const [name] = parseVersion(key);
         const installedVersion = assembleVersion(name, value.version);
 
@@ -27,8 +33,10 @@ export default class InstalledController extends Controller {
           ...acc,
           [installedVersion]: { ...def, packages: [...def.packages, key] }
         };
-      },
-      {}
-    );
+      }, {});
+  }
+
+  @action updateFilter(event) {
+    this.filter = event.target.value;
   }
 }
